c4d16d71208f8dbfd1635458030c5d049fbdf90e
Nandy Bâ <nandyba@yahoo.fr>
2023-11-18 17:00:45 +0300

    Get polygon ID info

--- server/server.js
+++ server/server.js
@@ -8,5 +8,15 @@ app.get("/", async (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/get-data", async (req, res) => {
+  const response = await fetch('https://api.wiw.io/public/user/profile/name/nandy');
+  const data = await response.json();
+  const { partner_claims } = data;
+  const polygonID_badges = data.partner_claims.polygon_id.badges
+  console.log({data})
+  console.log({partner_claims})
+  console.log({polygonID_badges})
+  res.send(data);
+});
 
 app.listen(3001);
