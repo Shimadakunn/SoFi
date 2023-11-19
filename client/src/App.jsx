df621e9a07e26b476103891f5a1896f2d44abc6e
Shimaadakunn <lcombaret@hotmail.co.jp>
2023-11-19 03:08:11 +0100

    mask integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,59 +1,43 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Hero from './Pages/Hero'
 import Main from './Pages/Main'
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider, darkTheme
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-  zora,
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import Deposit from './Pages/Deposit'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'viem/chains'
 
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
-  [
-    publicProvider()
-  ]
-);
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'f686c5da06280d4291e45e43fa7f0942',
-  chains
-});
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
+// 1. Get projectId
+const projectId = '653a9d4ac000e644b7472298be285b91'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains,themeVariables: {
+  '--w3m-accent': '#7b3fe4',
+} })
 
 function App() {
   return (
     <>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} theme={darkTheme({
-      accentColor: '#7b3fe4',
-      accentColorForeground: 'white',
-      borderRadius: 'small',
-      fontStack: 'system',
-      overlayBlur: 'small',
-    })} coolMode >
+    <WagmiConfig config={wagmiConfig}
+    >
       <Router>
       <Routes>
           <Route exact path="/" element={<Hero/>}/>
           <Route exact path="/main" element={<Main/>}/>
+          <Route exact path="/deposit" element={<Deposit/>}/>
         </Routes>
       </Router>
-      </RainbowKitProvider>
     </WagmiConfig>
     </>
   )
